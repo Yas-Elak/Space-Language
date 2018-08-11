39a08651b01f86b17429f30af2ff2097faf48bbb
Yassine El Alaoui Lakhnati <elalaoui87@gmail.com>
2018-08-11 21:09:35 +0200

    Only 1 class sprite now

--- core/src/com/yaselak/game/sprites/Sprites.java
+++ core/src/com/yaselak/game/sprites/Sprites.java
@@ -3,115 +3,146 @@ package com.yaselak.game.sprites;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
-/**
- * Created by Yassine on 08-03-18.
- */
 
 public class Sprites {
 
-    private int movement;
-    private int frameCols, frameRows;
-    private String frameLocation;
+    private float movement;
+    private String packLocation;
     private float frameDuration;
 
-    private Animation<TextureRegion> spriteAnimation;
-    private Texture spriteSheet;
-    private TextureRegion currentFrame;
+    private Animation spriteAnimation;
     private Vector2 position;
-    private float stateTime;
 
+    private Texture onlyTexture;
     private Rectangle bounds;
 
-    private int spriteWidth;
-    private int spriteHeight;
+    private float spriteOriginalWidth,spriteOriginalHeight, spriteDesiredWidth, spriteDesiredHeight;
 
-
-    //for the sprite who doesn't move and are Animated like the ennemi
-    public Sprites(String frameLocation, int frameCols, int frameRows, float frameDuration){
-        this.frameLocation = frameLocation;
-        this.frameCols = frameCols;
-        this.frameRows = frameRows;
+    //three constructor
+    // one for the sprites animated
+    public Sprites(float x, float y, float movement, String packLocation,
+                   float frameDuration, float pourcentSize) {
+        this.movement = movement;
+        position = new Vector2(x, y);
+        this.packLocation = packLocation;
         this.frameDuration = frameDuration;
-        GenerateSprite();
+        GenerateSpriteAtlas();
+        setSpriteSize(pourcentSize);
         bounds = new Rectangle(0, 0, 0, 0);
     }
-    // for the sprites who moves and are animated, the ship move even if doesn't seems to
-    public Sprites(float x, float y, int movement, String frameLocation, int frameCols, int frameRows, float frameDuration){
+    // One for the sprite who are not animated (OnlyTexture) and needs bounds
+    public Sprites(float x, float y, float movement, String textureLocation, float pourcentSize) {
+        this(x,y,movement,textureLocation);
+        setSpriteSize(pourcentSize);
+        bounds = new Rectangle(0, 0, 0, 0);
+    }
+    //One for the sprite who are not animated (OnlyTexture) and doesn't need bounds
+    public Sprites(float x, float y, float movement, String textureLocation) {
         this.movement = movement;
-        this.frameLocation = frameLocation;
-        this.frameCols = frameCols;
-        this.frameRows = frameRows;
-        this.frameDuration = frameDuration;
         position = new Vector2(x, y);
-        GenerateSprite();
-        bounds = new Rectangle(position.x, position.y, spriteWidth, spriteHeight);
+        onlyTexture = new Texture(Gdx.files.internal(textureLocation));
+        this.spriteOriginalWidth = onlyTexture.getWidth();
+        this.spriteOriginalHeight = onlyTexture.getHeight();
     }
 
-    public void updateMovement(float dtx, float dty){
-        position.add(movement * dtx,dty);
-        bounds.setPosition(position.x, position.y);
+    //Generate the animation for the first constructor
+    public void GenerateSpriteAtlas() {
+        TextureAtlas textureAtlas = new TextureAtlas(Gdx.files.internal(packLocation));
+        this.spriteOriginalWidth = textureAtlas.getRegions().get(1).originalWidth;
+        this.spriteOriginalHeight = textureAtlas.getRegions().get(1).originalHeight;
+        spriteAnimation = new Animation(frameDuration, textureAtlas.getRegions());
     }
-    //surcharge de méthode pour la balle
-    public void updateMovement(float dtx, float dty, boolean shooted, int acceleration){
-        if (!shooted)
-            position.add(movement * dtx,dty);
-        else
-            position.add(acceleration * dtx,dty);
-        bounds.setPosition(position.x, position.y);
-    }
-    public void AnimeMe(boolean loop){
-        if (loop) {
-            stateTime += Gdx.graphics.getDeltaTime();
-        }
-        currentFrame = spriteAnimation.getKeyFrame(stateTime, true);
-    }
-    public void GenerateSprite(){
-        spriteSheet = new Texture(Gdx.files.internal(frameLocation));
-        TextureRegion[][] tmp = TextureRegion.split(spriteSheet,
-                spriteSheet.getWidth() / frameCols,
-                spriteSheet.getHeight() / frameRows);
-        TextureRegion[] spriteFrames = new TextureRegion[frameCols * frameRows];
-        int index = 0;
-        for (int i = 0; i < frameRows; i++) {
-            for (int j = 0; j < frameCols; j++) {
-                spriteFrames[index++] = tmp[i][j];
-            }
-        }
-        spriteAnimation = new Animation<TextureRegion>(frameDuration, spriteFrames);
-        stateTime = 0f;
-        spriteWidth = spriteSheet.getWidth()/ frameCols;
-        spriteHeight = spriteSheet.getHeight()/ frameCols;
 
+    public void setBoundsSize(float x, float y, float width, float height) {
+        bounds.setPosition(x,y);
+        bounds.setWidth(width);
+        bounds.setHeight(height);
     }
-    public Vector2 getPosition() {
-        return position;
+
+    public void updateMovement(float dtx, float dty) {
+        position.add(movement * dtx, dty);
+        if (bounds != null) bounds.setPosition(position.x, position.y);
     }
-    public TextureRegion getCurrentFrame() {
-        return currentFrame;
+
+    //OverWrite for the bullet who change the movement
+    public void updateMovement(float dtx, float dty, boolean shooted, float acceleration) {
+        if (!shooted) position.add(movement * dtx, dty);
+        else position.add(acceleration * dtx, dty);
+        bounds.setPosition(position.x, position.y);
     }
-    public int getSpriteWidth() {
-        return spriteWidth;
+
+    public void reposition(float x, float y) {
+        position.set(x, y);
     }
-    public int getSpriteHeight() {
-        return spriteHeight;
+
+    public boolean collides(Rectangle player) {
+        return player.overlaps(bounds);
     }
+
+    public void hideBullet() {
+        this.onlyTexture = new Texture("spritespaceshipgame/bullethidden.png");
+    }
+    public void unHideBullet(){
+        this.onlyTexture = new Texture("spritespaceshipgame/bullet.png");
+    }
+
+    public void setSpriteSize(float constante) {
+        //original height / original width) x new width = new height
+            this.spriteDesiredWidth = constante;
+            this.spriteDesiredHeight = (spriteOriginalHeight / spriteOriginalWidth) * spriteDesiredWidth;
+    }
+    //----------------------------Getter and setter-------------------------------------------------
     public Animation<TextureRegion> getSpriteAnimation() {
         return spriteAnimation;
     }
-    public float getStateTime() {
-        return stateTime;
+
+    public float getSpriteDesiredWidth() {
+        return spriteDesiredWidth;
     }
+    public float getSpriteDesiredHeight() {
+        return spriteDesiredHeight;
+    }
+
+    public void setSpriteOriginalWidth(int spriteOriginalWidth) {
+        this.spriteOriginalWidth = spriteOriginalWidth;
+    }
+    public float getSpriteOriginalWidth() {
+        return spriteOriginalWidth;
+    }
+
+    public void setSpriteOriginalHeight(int spriteOriginalHeight) {
+        this.spriteOriginalHeight = spriteOriginalHeight;
+    }
+    public float getSpriteOriginalHeight() {
+        return spriteOriginalHeight;
+    }
+
     public Rectangle getBounds() {
         return bounds;
     }
-    public void setStateTime(float stateTime) {
-        this.stateTime = stateTime;
+
+    public Texture getOnlyTexture() {
+        return onlyTexture;
     }
-    public void reposition(float x, float y){
-        position.set(x, y);
+
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    public float getMovement() {
+        return movement;
+    }
+    public void setMovement(float movement) {
+        this.movement = movement;
+    }
+
+    public void dispose(){
+        if (onlyTexture!= null) onlyTexture.dispose();
     }
 }
+
